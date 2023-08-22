36a777e2553de8b490d6bea83fa19c0736cb516c
donguramee <sanulim94@gmail.com>
2023-08-22 15:49:41 +0900

    Add: DAY8, DAY9 [PJW]

---
+++ PJW/DAY8/01_배열자르기.js
@@ -0,0 +1,13 @@
+// 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(numbers, num1, num2) {
+  let answer = [];
+  answer = numbers.slice(num1, num2 + 1);
+  return answer;
+}
+
+// 테스트 1 〉	통과 (0.03ms, 33.4MB)
+// 테스트 2 〉	통과 (0.03ms, 33.5MB)
+// 테스트 3 〉	통과 (0.03ms, 33.4MB)
+// 테스트 4 〉	통과 (0.03ms, 33.4MB)
+// 테스트 5 〉	통과 (0.04ms, 33.4MB)
