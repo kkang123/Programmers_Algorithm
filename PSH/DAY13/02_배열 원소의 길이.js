8c0de5532d029261232fbe5fec32cbe87ce92d49
sohee <sthgml@naver.com>
2023-08-27 23:56:05 +0900

    Add: DAY12, DAY13 [PSH]

---
+++ PSH/DAY13/02_배열 원소의 길이.js
@@ -0,0 +1,9 @@
+function solution(strlist) {
+    return strlist.map(v=>v.length);
+}
+// 테스트 1 〉	통과 (0.03ms, 33.4MB)
+// 테스트 2 〉	통과 (0.03ms, 33.4MB)
+// 테스트 3 〉	통과 (0.03ms, 33.4MB)
+// 테스트 4 〉	통과 (0.03ms, 33.4MB)
+// 테스트 5 〉	통과 (0.03ms, 33.5MB)
+// 테스트 6 〉	통과 (0.03ms, 33.4MB)
