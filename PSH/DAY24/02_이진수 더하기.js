de6e9d74cac730060650c21aeb6cb47c25028396
sohee <sthgml@naver.com>
2023-10-11 12:05:28 +0900

    Add: DAY24 [PSH]

---
+++ PSH/DAY24/02_이진수 더하기.js
@@ -0,0 +1,14 @@
+function solution(bin1, bin2) {
+    let number = parseInt(bin1,2)+parseInt(bin2,2)
+    return number.toString(2);
+}
+
+// 테스트 1 〉	통과 (0.03ms, 33.4MB)
+// 테스트 2 〉	통과 (0.03ms, 33.4MB)
+// 테스트 3 〉	통과 (0.03ms, 33.4MB)
+// 테스트 4 〉	통과 (0.04ms, 33.5MB)
+// 테스트 5 〉	통과 (0.03ms, 33.4MB)
+// 테스트 6 〉	통과 (0.03ms, 33.5MB)
+// 테스트 7 〉	통과 (0.03ms, 33.5MB)
+// 테스트 8 〉	통과 (0.03ms, 33.5MB)
+// 테스트 9 〉	통과 (0.03ms, 33.5MB)
